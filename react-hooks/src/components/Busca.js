ab5bed2bdbe81963be55cebbe86de927255d8cad
Rodrigo Bossini <professorbossini@gmail.com>
2024-11-12 19:18:44 -0300

    ap08(hooks): fim da secao 2.18

--- react-hooks/src/components/Busca.js
+++ react-hooks/src/components/Busca.js
@@ -1,21 +1,33 @@
 // rafce
 import React, { useEffect, useState } from 'react'
+import { Button } from 'primereact/button'
 import { IconField } from 'primereact/iconfield'
 import { InputIcon } from 'primereact/inputicon'
 import { InputText } from 'primereact/inputtext'
+import axios from 'axios'
+import striptags from 'striptags'
 
 const Busca = () => {
-  const [termoDeBusca, setTermoDeBusca] = useState('')
+  const [termoDeBusca, setTermoDeBusca] = useState('React')
+  const [resultados, setResultados] = useState([])
+  
   useEffect(() => {
-    console.log('Executando todas as vezes...')
-  })
-
-  useEffect(() => {
-    console.log('Executando somente uma vez...')
-  }, [])
-
-  useEffect(() => {
-    console.log('Executando somente quando o termo  é atualizado...')
+    const fazBusca = async () => {
+      const { data } = await axios.get(
+        'https://en.wikipedia.org/w/api.php', {
+          params: {
+            action: 'query',
+            list: 'search',
+            format: 'json',
+            origin: '*',
+            srsearch: termoDeBusca
+          }
+        }
+      )
+      setResultados(data.query.search)
+    }
+    if(termoDeBusca)
+      fazBusca()
   }, [termoDeBusca])
   return (
     <div>
@@ -25,6 +37,31 @@ const Busca = () => {
           value={termoDeBusca} 
           onChange={(e) => setTermoDeBusca(e.target.value)}/>
       </IconField>
+      {
+        resultados.map(resultado => (
+          <div
+            key={resultado.pageid}
+            className='my-2 border border-1 border-400'>
+              <h4
+                className='p-1 text-center border-bottom border-bottom-1 border-400'>
+                  {resultado.title}
+                  <span>
+                    <Button 
+                      icon="pi pi-send"
+                      className='ml-2 p-button-rounded'
+                      onClick={() => window.open(
+                        `https://en.wikipedia.org?curid=${resultado.pageid}`,
+                      )}
+                    />
+                  </span>
+              </h4>
+              <div className='p-2'>
+                <span>{striptags(resultado.snippet)}</span>
+                <span>...</span>
+              </div>
+          </div>
+        ))
+      }
     </div>
   )
 }
